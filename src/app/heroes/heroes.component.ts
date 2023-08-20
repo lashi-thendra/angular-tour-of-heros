4cb995bb39b8502090a83df6604c0a29d465582a
Lashi Thendra <lashithendrabandara@gmail.com>
2023-08-20 12:39:53 +0530

    add class binding to buttons

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,18 +2,18 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
-// component is a decorator function
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
 
+export class HeroesComponent {
+
+  heroes = HEROES;
   selectedHero?: Hero;
-  onSelect(hero:Hero):void {
+
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
 }
